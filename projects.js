72d38d0b9ff70c82f2a3f48e1dfe020b4403320d
akb <doge.wow.so.scare@gmail.com>
2020-04-23 12:28:50 +0900

    feat: Enable scrolling to DOM anchor with scrollBehavior() of Vue-Router

--- projects.js
+++ projects.js
@@ -5,15 +5,16 @@ var ProjectTable = Vue.component("project", {
   template: `
         <v-content>
             <v-container>
-            <v-card elevation="20" class="my-10">
-                <v-card-title class="display-1 mx-4 py-8"
-                >プロジェクト一覧</v-card-title
-                >
-                <v-card-subtitle class="ml-4 subtitle-1"
-                >ポートフォリオサイトへのご訪問ありがとうございます！ここ一年ほどの私の作品を一部ご紹介します。<br>
-                各プロジェクトのデモページと、プロジェクトを詳細に解説したGitHubのREADMEへのリンク先も貼りましたので、ぜひご覧ください。</v-card-subtitle
-                >
-                <v-card-text>
+            <v-card elevation="10" class="mt-10 px-4">
+              <v-card-title class="display-1 py-8">プロジェクト一覧</v-card-title>
+                <v-card-subtitle class="subtitle-1">
+                ポートフォリオサイトへのご訪問ありがとうございます！ここ一年ほどの私の作品を一部ご紹介します。<br>
+                各プロジェクトのデモページと、プロジェクトを詳細に解説したGitHubのREADMEへのリンク先も貼りましたので、ぜひご覧ください。
+              </v-card-subtitle>            
+            </v-card>
+            <v-card elevation="10" class="my-8">
+              <v-card-title class="headline">目次</v-card-title>
+              <v-card-text>
                 <v-data-table
                     :headers="projectHeaders"
                     :items="projects"
@@ -21,7 +22,7 @@ var ProjectTable = Vue.component("project", {
                     :items-per-page="20"
                 >
                     <template v-slot:item.name="{item}">
-                    <b>{{item.name}}</b>
+                    <b><router-link :to="addSharp(item.identifier)">{{item.name}}</router-link></b>
                     </template>
                     <template v-slot:item.desc="{item}">
                     <p class="my-3"><span v-html="item.desc"></span></p>
@@ -83,14 +84,19 @@ var ProjectTable = Vue.component("project", {
                 </v-data-table>
                 </v-card-text>
             </v-card>
-            </v-container> </v-content
-        >`,
+            <v-card v-for="(project, index) in projects" elevation="10" class="my-8" >
+              <v-card-title :id="project.identifier">{{project.name}}</v-card-title>
+
+            </v-card>
+          </v-container>
+        </v-content>`,
   data: function () {
     return {
       msg: "hello, world",
       projects: [
         {
           name: "仙台高齢化地域の可視化",
+          identifier: "ageing",
           desc:
             "仙台の町ごとの高齢人口比率をヒートマップで表示します。一市民として興味の湧く事柄なので、個人的に気に入っているプロジェクトです。Pandasのデータフレーム操作の便利さに感動しました。",
           demourl: "https://azureleaf.github.io/ageing-sendai/",
@@ -100,6 +106,7 @@ var ProjectTable = Vue.component("project", {
         },
         {
           name: "学生寮情報管理システム",
+          identifier: "dorm",
           desc:
             "学生寮の会計機能・当番割振機能・情報投稿機能をもったWebサービス。一番本格的で実用的なプロジェクト。",
           demourl: "",
@@ -109,15 +116,27 @@ var ProjectTable = Vue.component("project", {
         },
         {
           name: "激強五目並べ",
+          identifier: "gomoku_vue",
           desc:
-            "コンピュータを相手に戦う五目並べです。COM側がめちゃくちゃ強くて、ほぼ勝てない。私がWeb系の勉強をはじめて最初に作った作品なので、思い入れがあります。",
+            "コンピュータを相手に戦う五目並べです。COM側がめちゃくちゃ強くて、ほぼ勝てない。",
+          demourl: "",
+          githuburl: "",
+          frameworks: ["vue"],
+          backends: [],
+        },
+        {
+          name: "激強五目並べ（React版）",
+          identifier: "gomoku_react",
+          desc:
+            "上記の五目並べのReact版です。私がWeb系の勉強をはじめて最初に作ったものなので、思い入れがあります。",
           demourl: "",
           githuburl: "https://github.com/azureleaf/gomoku",
-          frameworks: ["vue", "react"],
+          frameworks: ["react"],
           backends: [],
         },
         {
           name: "三角形の五心",
+          identifier: "triangle",
           desc:
             "マウスで三角形を描くと、三角形の重心・内心・外心・傍心・垂心を自動で描画してくれます。幾何学的できれい。Canvasを使ってマウスなどとのインタラクションを勉強しました。",
           demourl:
@@ -128,15 +147,17 @@ var ProjectTable = Vue.component("project", {
         },
         {
           name: "仙台バス停・地下鉄検索ボット",
+          identifier: "line",
           desc:
             "LINEのボット。彼にGPSの現在位置を投げると、一番近くにあるバス停と地下鉄の駅を検索して返してくれます。Herokuで稼働中。本当はバス発車時刻も提示してほしかったが、仙台市交通局がそのデータを公開していなかった。",
           demourl: "",
           githuburl: "https://github.com/azureleaf/greet_bot",
           frameworks: ["py"],
-          backends: ["Flask + Firebase"],
+          backends: ["Flask"],
         },
         {
           name: "WiFiストーカー",
+          identifier: "stalker",
           desc:
             "LANを定期的に自動スキャンし、接続中のデバイスのMACアドレスを取得します。他の人のスマホやPCの接続有無をもとにその人の在室状況を追跡しようとする、嫌らしいプログラム。",
           demourl: "",
@@ -146,15 +167,17 @@ var ProjectTable = Vue.component("project", {
         },
         {
           name: "通院アシスタント",
+          identifier: "hospital",
           desc:
             "近所にある某病院は予約制で、病院ウェブサイトでリアルタイムの診察状況を公開しています。このスクリプトはそのウェブサイトから現在の混雑状況を自動取得し、また病院ウェブサイト上での受診予約まで自動でやってくれる優れものです。ウェブスクレイピングとブラウザ操作自動化の練習台。",
           demourl: "",
           githuburl: "https://github.com/azureleaf/hospital_latency",
           frameworks: ["py", "vue", "js"],
-          backends: ["Express.js + TypeORM + MySQL"],
+          backends: [],
         },
         {
           name: "日本の大都市で気候が一番良いのはどこだ？",
+          identifier: "climate",
           desc:
             "地理データの可視化シリーズ。日本７大都市の気温・降水量・日照を比較し、仙台がベストだということを証明したい。",
           demourl: "",
@@ -163,9 +186,9 @@ var ProjectTable = Vue.component("project", {
           backends: [],
         },
         {
-          name: "リアルタイム掲示板",
-          desc:
-            "socket.io",
+          name: "リアルタイムチャット",
+          identifier: "chat",
+          desc: "socket.io",
           demourl: "",
           githuburl: "",
           frameworks: ["js", "vue"],
@@ -173,6 +196,7 @@ var ProjectTable = Vue.component("project", {
         },
         {
           name: "フラクタルでパセリ描画",
+          identifier: "fractal",
           desc:
             "再帰表現を使って図形を描画します。HTML Canvasの勉強のため作成。",
           demourl: "https://azureleaf.github.io/canvas/fractal/canvastree.html",
@@ -182,6 +206,7 @@ var ProjectTable = Vue.component("project", {
         },
         {
           name: "プログラミング学習ノート",
+          identifier: "notes",
           desc:
             "プログラミングについての膨大な勉強メモをマークダウンにまとめたものです。特に<a href='https://github.com/azureleaf/study-notes/blob/master/js/promise.md'>Promiseについて</a>は結構まとめました。",
           demourl: "",
@@ -191,6 +216,7 @@ var ProjectTable = Vue.component("project", {
         },
         {
           name: "Express.js練習台",
+          identifier: "express",
           desc:
             "バックエンドの練習台。特に何かサービスの形にはなっていませんが、Expressの基本的な機能、Axios、ORM、種々のDBにさらっと触れるためのレポジトリ。",
           demourl: "",
@@ -200,6 +226,7 @@ var ProjectTable = Vue.component("project", {
         },
         {
           name: "ログイン機能をつくる",
+          identifier: "user_login",
           desc:
             "知人のGitHubレポジトリへの私のcontribution。Axiosでの通信、非同期処理、ORMでのDBへのアクセス、Bcryptによるハッシュ化、トークンの保存などを、協力しながら一つ一つ丁寧に実装しました。",
           demourl: "",
@@ -209,6 +236,7 @@ var ProjectTable = Vue.component("project", {
         },
         {
           name: "ポートフォリオサイト",
+          identifier: "portfolio",
           desc:
             "このサイトそのもの。単なる静的サイトなので速攻で終わると思っていたら、CDNでVue-Routerを使うのはわりと苦戦しました。それにしてもVuetifyのマテリアルデザインはきれい。Font Awesomeのアイコン充実化に寄与しているデザイナーにも頭があがらない。",
           demourl: "https://azureleaf.github.io/",
@@ -255,6 +283,9 @@ var ProjectTable = Vue.component("project", {
   methods: {
     isUsing: function (frameworks, framework) {
       return frameworks.includes(framework);
+    },
+    addSharp: function (str) {
+      return "#" + str;
     },
   },
 });
